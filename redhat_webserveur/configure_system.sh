08dfb61120c495e13f48811e620588ecec5ec45e
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-12-27 21:54:50 +0100

    Update

--- redhat_webserveur/configure_system.sh
+++ redhat_webserveur/configure_system.sh
@@ -8,19 +8,36 @@ echo "Configuration du système..."
 systemctl enable --now httpd
 
 systemctl start mysqld
-TEMP_PASS=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-mysql --connect-expired-password -u root -p"$TEMP_PASS" <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'P@sswordTemporaireFort123!';
-UNINSTALL COMPONENT 'file://component_validate_password';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '1';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-DELETE FROM mysql.user WHERE User='';
+MYSQL_ROOT_PWD="SuperMdp2026sql!"
+
+# 1. Définir le mot de passe root (si pas encore fait)
+mysql -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';
 FLUSH PRIVILEGES;
 EOF
 
-systemctl enable --now mysqld
+# 2. Supprimer utilisateurs anonymes
+mysql -u root -p"${MYSQL_ROOT_PWD}" <<EOF
+DELETE FROM mysql.user WHERE User='';
+EOF
+
+# 3. Désactiver l'accès root à distance
+mysql -u root -p"${MYSQL_ROOT_PWD}" <<EOF
+DELETE FROM mysql.user WHERE User='root' AND Host!='localhost';
+EOF
+
+# 4. Supprimer la base de test
+mysql -u root -p"${MYSQL_ROOT_PWD}" <<EOF
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db LIKE 'test\\_%';
+EOF
+
+mysql -u root -p"${MYSQL_ROOT_PWD}" <<EOF
+FLUSH PRIVILEGES;
+EOF
+
+systemctl enable mysqld
 
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --reload
